fbe68ece0d3d514765f437e55828e4df1b2eb72b
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-06-24 21:30:12 -0700

    Add files via upload

---
+++ firstfunctions.py
@@ -0,0 +1,13 @@
+ def hello():
+    print("hello")
+
+def pack():
+      food="sandwich",
+      snack="chips"
+      drink="soda"
+      return (food,snack,drink)
+
+
+
+def eat_lunch():
+      for i in pack(): print("first i eat"+ i)
